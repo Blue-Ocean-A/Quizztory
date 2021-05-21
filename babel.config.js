1d5063da79cdb9db23abf4c8749935559821fa57
Julian Bowman <julianfbowman@gmail.com>
2021-05-21 14:39:10 -0600

    installed AirBnB eslint

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    presets: [
-      '@babel/preset-env',
-      '@babel/preset-react',
-    ],
-    plugins: [
-      '@babel/plugin-syntax-jsx',
-    ],
-  };
+  presets: [
+    '@babel/preset-env',
+    '@babel/preset-react',
+  ],
+  plugins: [
+    '@babel/plugin-syntax-jsx',
+  ],
+};
