be20a1bd38f782d38512d56ba7f85126cfcc32f2
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-05-22 15:34:38 -0600

    finishes db.init.js seeding, need to add indexing

--- db.init.js
+++ db.init.js
@@ -1,20 +1,56 @@
 const fs = require('fs');
+const mongoose = require('mongoose');
+const Credential = require('./db/models/Credential');
+const Profile = require('./db/models/Profile');
+const Quiz = require('./db/models/Quiz');
+const QuizData = require('./db/models/QuizData');
 
-const mongoPort = 'mongodb://localhost:27017';
-const mongoURI = mongoPort + '/quizztory';
-const dbName = 'quizztory'
+const mongoURI = 'mongodb://localhost:27017/quizztory';
 
-const conn = new Mongo();
-const dbs = conn.adminCommand('listDatabases');
+const credentialsJson = fs.readFileSync('./data/credentials.json');
+const credentials = JSON.parse(credentialsJson);
 
-let db = null;
+const profilesJson = fs.readFileSync('./data/profiles.json');
+const profiles = JSON.parse(profilesJson);
 
-for(let i = 0; i < dbs.length; i++) {
-  if(dbs[i].name === dbName) {
-    db = conn.getDB(dbName);
-    db.dropDatabase();
-    break;
-  }
-}
+const quizsJson = fs.readFileSync('./data/quizzes.json');
+const quizzes = JSON.parse(quizsJson);
 
-db = conn.getDB(dbName);
+const quizdatasJson = fs.readFileSync('./data/quizdatas.json');
+const quizdatas = JSON.parse(quizdatasJson);
+
+const db = mongoose.connect(mongoURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+})
+  .then((db) => {
+    Credential.insertMany(credentials, (err, results) => {
+      if (err) {
+        throw new Error('error seeding credentials');
+      } else {
+        Profile.insertMany(profiles, (err, results) => {
+          if (err) {
+            throw new Error('error seeding credentials');
+          } else {
+            Quiz.insertMany(quizzes, (err, results) => {
+              if (err) {
+                throw new Error('error seeding credentials');
+              } else {
+                QuizData.insertMany(quizdatas, (err, results) => {
+                  if (err) {
+                    throw new Error('error seeding credentials');
+                  } else {
+                    console.log('FINISHED SEEDING DATABASE, please exit node');
+                  }
+                });
+              }
+            });
+          }
+        });
+      }
+    });
+  })
+  .catch((err) => {
+    console.log(`There was a problem connecting to mongo at: ${mongoURI} for seeding`);
+    console.log(err);
+  });
