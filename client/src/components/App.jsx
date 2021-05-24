8bd53e104fbd604c8daf71500f3c53a7a2f668ea
Tamir Amitai <tamir@Tamirs-MacBook-Air.local>
2021-05-24 15:30:36 -0600

    merged with main after kim updates

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,8 +1,7 @@
 /* eslint-disable import/extensions */
 import React, { useState } from 'react';
 import { ThemeProvider, makeStyles } from '@material-ui/core/styles';
-import { Grid, Container } from '@material-ui/core';
-import theme from '../theme.js';
+import { Grid, Box, Button } from '@material-ui/core';
 import QuizztoryLogo from '../../../QuizztoryLogo.png';
 import Login from './Login.jsx';
 import Create from './Create.jsx';
@@ -10,9 +9,14 @@ import Friends from './Friends.jsx';
 import FriendsResults from './FriendsResults.jsx';
 import Quiz from './Quiz.jsx';
 import QuizList from './QuizList.jsx';
+// import Score from './Score.jsx';
 import SignUp from './SignUp.jsx';
+import historyEasy from '../quizData/historyEasy.js';
 
 const useStyles = makeStyles((theme) => ({
+  container: {
+    marginLeft: 200,
+  },
   root: {
     backgroundColor: theme.palette.primary.light,
     dispay: 'flex',
@@ -27,6 +31,13 @@ const useStyles = makeStyles((theme) => ({
   },
   quizzList: {
   },
+  createButton: {
+    backgroundColor: theme.palette.primary.dark,
+    color: theme.palette.primary.light,
+    dispay: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+  },
 }));
 
 const App = () => {
@@ -34,13 +45,12 @@ const App = () => {
   const [allUsers, setAllUsers] = useState([]);
   const [allQuizzes, setAllQuizzes] = useState([historyEasy]);
   const [currentQuiz, setCurrentQuiz] = useState(historyEasy);
-  const [currentUser, setCurrentUser] = useState('Kim');
+  const [currentUser, setCurrentUser] = useState();
   const [userName, setUserName] = useState('');
   const [password, setPassword] = useState('');
   const [display, setDisplay] = useState('quiz');
 
   const classes = useStyles();
-
   if (display === 'login') {
     return (
       <>
@@ -64,6 +74,14 @@ const App = () => {
         <Grid container spacing={2} className={classes.root}>
           <Grid item xs={6}>
             <QuizList allQuizzes={allQuizzes} />
+            <Button
+              variant="contained"
+              aria-label="create"
+              className={classes.createButton}
+              onClick={() => { setDisplay('create'); }}
+            >
+              Create A Quiz
+            </Button>
           </Grid>
           <Grid item xs={2}>
             <Friends />
@@ -74,14 +92,19 @@ const App = () => {
   }
 
   if (display === 'signUp') {
-    <SignUp
-      setDisplay={setDisplay}
-      setCurrentUser={setCurrentUser}
-      userName={userName}
-      setUserName={setUserName}
-      password={password}
-      setPassword={setPassword}
-    />;
+    return (
+      <>
+        <img className={classes.image} src={QuizztoryLogo} alt="Quizztory" />
+        <SignUp
+          setDisplay={setDisplay}
+          setCurrentUser={setCurrentUser}
+          userName={userName}
+          setUserName={setUserName}
+          password={password}
+          setPassword={setPassword}
+        />
+      </>
+    );
   }
 
   if (display === 'quiz' || display === 'quizResults') {
@@ -91,9 +114,21 @@ const App = () => {
         <Grid container spacing={2} className={classes.root}>
           <Quiz
             currentQuiz={currentQuiz}
-            currentUser={currentUser}
             display={display}
-            setDisplay={setDisplay}
+            setDisplay={currentUser}
+          />
+        </Grid>
+      </>
+    );
+  }
+
+  if (display === 'create') {
+    return (
+      <>
+        <img className={classes.image} src={QuizztoryLogo} alt="Quizztory" />
+        <Grid container spacing={2} className={classes.root}>
+          <Create
+            setDisplay={currentUser}
           />
         </Grid>
       </>
