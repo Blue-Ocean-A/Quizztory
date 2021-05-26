df7ccbe270c61b92771d07dda5013aaaa8f4d62a
Kim Schaefer <kim.s.schaefer@gmail.com>
2021-05-26 12:12:03 -0600

    Fixed 'back' button bug on Quiz.jsx

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -134,7 +134,7 @@ const App = () => {
           <Quiz
             currentQuiz={currentQuiz}
             display={display}
-            setDisplay={currentUser}
+            setDisplay={setDisplay}
           />
         </Grid>
       </>
