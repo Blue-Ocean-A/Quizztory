5bcea1d87dc633688d2a016a8584347a49422a97
Julian Bowman <julianfbowman@gmail.com>
2021-05-27 13:50:41 -0600

    fixed login button

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -51,8 +51,8 @@ const Login = ({
     e.preventDefault();
 
     axios.get(`/api/user?name=${userName}&password=${password}`)
-      .then(() => {
-        setCurrentUser(userName);
+      .then((response) => {
+        setCurrentUser(response.data[0].name);
         setDisplay('home');
       })
       .catch(() => {
